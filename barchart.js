74a51db19d9d6b589c1a6acd8749874c14a42621
Théophile Mot <theophile.mot@gmail.com>
2018-07-04 19:59:26 -0230

    first commit

---
+++ barchart.js
@@ -0,0 +1,13 @@
+function drawBarChart(data, options, element) {
+  // data:
+  //   [ {x, height, colour}, ... ]
+  // options:
+  //   {defaultColour, barWidth, }
+
+  /*let barWidth = options.barWidth || 10;
+
+  for (let d of data) {
+    let x = d.x;
+    let y = 0;
+  }*/
+}
