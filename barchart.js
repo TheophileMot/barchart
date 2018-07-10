9cfd969d4d7609951dd419d9005838a8fed280be
Théophile Mot <theophile.mot@gmail.com>
2018-07-09 20:25:42 -0400

    create bars as DIV elements; simple animation

--- barchart.js
+++ barchart.js
@@ -1,13 +1,23 @@
+$( document ).ready(function() {
+  drawBarChart([1, 7, 3, 1, 2, 5, 6, 8], {}, ".chart");
+});
+
 function drawBarChart(data, options, element) {
   // data:
   //   [ {x, height, colour}, ... ]
   // options:
   //   {defaultColour, barWidth, }
 
-  /*let barWidth = options.barWidth || 10;
+  /*let barWidth = options.barWidth || 10;*/
 
-  for (let d of data) {
-    let x = d.x;
-    let y = 0;
-  }*/
+  for (let i in data) {
+    let height = data[i];
+    let barId = "bar" + i;
+    $( element ).append(
+      '<div id="' + barId + '" style="width: 10px; height: 0px; background-color: darkslategrey; position: absolute; bottom: 10px; left: ' + (+i + 1) * 20 + 'px;"></div>'
+    );
+    $( "#" + barId ).animate({
+      height: 10 * height + "px",
+    })
+  }
 }
