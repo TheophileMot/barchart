252707f3e4d6e8597e50ccc6f2c1824f883905aa
Timothy Mott <tjbmott+github@gmail.com>
2018-07-13 13:53:40 -0400

    vertical bars (incl. negative) spaced appropriately

--- barchart.js
+++ barchart.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
-  drawBarChart([1, 7, 3, 1, 2, 5, 6, 8, 1, 1, 1, 18, 2], {}, '.chart');
+  drawBarChart([1, 7, 3, 1, 2, 5, -10, 8, 1, 1, 1, 18, 2], {}, '.chart');
+  drawBarChart([2, -3, -4, -5], {}, '.chart');
 });
 
 const BAR_CHART_DEFAULTS = {
   barGapRatio: 0.6, // bars slightly wider than gaps
-  padding: 15,
+  padding: 10,
 };
 
 class BarChart {
@@ -16,27 +17,38 @@ class BarChart {
     this.padding = (options.padding >= 0) ? options.padding : BAR_CHART_DEFAULTS.padding;
     this.barGapRatio = (options.barGapRatio > 0 && this.barGapRatio <= 1) ? options.barGapRatio : BAR_CHART_DEFAULTS.barGapRatio;
 
+    this.numBars = data.length;
+
     // set dimensions of chart, excluding padding
     this.width = $( element ).width() - 2 * this.padding;
     this.height = $( element ).height() - 2 * this.padding;
 
-    this.numBars = data.length;
-
+    // set properties derived from dimensions
     this.barSpacing = this.calcBarSpacing();
     this.barWidth = this.barSpacing * this.barGapRatio;
+    this.maxHeight = Math.max(0, ...this.data);
+    this.minHeight = Math.min(0, ...this.data);
     this.verticalScale = this.calcVerticalScale(element);
+    this.xAxisFromBottom = this.padding + (-this.minHeight) * this.verticalScale;
+    this.xAxisFromTop = this.padding + this.maxHeight * this.verticalScale;
   }
 
   draw() {
     for (let i in this.data) {
       let height = this.data[i];
       let barId = 'bar' + i;
-      let leftPos = this.padding + i * this.barSpacing;
+      let horizontalPos = 'left: ' + (this.padding + i * this.barSpacing) + 'px; width: ' + this.barWidth + 'px; ';
+      let verticalPos;
+      if (height >= 0) {
+        verticalPos = 'bottom: ' + this.xAxisFromBottom + 'px; height: 0px; ';
+      } else {
+        verticalPos = 'top: ' + this.xAxisFromTop + 'px; height: 0px; ';
+      }      
       $( this.element ).append(
-        '<div id="' + barId + '" style="width: ' + this.barWidth + 'px; height: 0px; background-color: darkslategrey; position: absolute; bottom: 0px; left: ' + leftPos + 'px;"></div>'
+        '<div id="' + barId + '" style="background-color: lightgoldenrodyellow; position: absolute; ' + horizontalPos + verticalPos + '"></div>'
       );
       $( '#' + barId ).animate({
-        height: 10 * height + 'px',
+        height: Math.abs(height) * this.verticalScale + 'px', // make sure to take absolute value of height in case bar is negative
       });
     }  
   }
@@ -50,11 +62,13 @@ class BarChart {
   //  calculate scale for transforming y values to actual pixels:
   //    this depends on the heights of the bars; if all bars are positive (resp. negative), the lowest (resp. greatest) height is 0
   calcVerticalScale() {
-    let maxHeight = Math.max(0, ...this.data);
-    let minHeight = Math.min(0, ...this.data);
-    let heightDiff = maxHeight - minHeight || 1;  // avoid division by zero
-
+    let heightDiff = this.maxHeight - this.minHeight || 1;  // avoid division by zero
     return this.height / heightDiff;
+  }
+
+  //  transform on y-axis from bar to element, including padding
+  verticalTransform(y) {
+    return this.padding + y * this.verticalScale - this.minHeight;
   }
 }
 
@@ -64,9 +78,6 @@ function drawBarChart(data, options, element) {
   // options:
   //   {defaultColour, barWidth, }
   
-  //$( element ).width(determineChartWidth(data, options));
-  //$( element ).height(determineChartHeight(data, options));
-
   let barChart = new BarChart(data, options, element);
   $( element ).prop('barChart', barChart);
 
